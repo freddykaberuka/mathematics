2249b061050c385a0dc2724ed52e2af1eb4074cd
freddykaberuka <freddykaberuka2009@gmail.com>
2023-09-15 10:37:43 +0200

    added calculator component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
+import Calculator from './components/Calculator';
 
 function App() {
   return (
     <div className="App">
-      <h1>React App</h1>
-      <p>Welcome to my Calculator App</p>
+      <Calculator />
     </div>
   );
 }
---
+++ src/components/digits.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const Digits = () => (
+  <div className="calculator-lower">
+    <div className="calculator-number">
+      <button type="button" alt="">7</button>
+      <button type="button" alt="">8</button>
+      <button type="button" alt="">9</button>
+    </div>
+    <div className="calculator-number">
+      <button type="button" alt="">4</button>
+      <button type="button" alt="">5</button>
+      <button type="button" alt="">6</button>
+    </div>
+    <div className="calculator-number">
+      <button type="button" alt="">1</button>
+      <button type="button" alt="">2</button>
+      <button type="button" alt="">3</button>
+    </div>
+    <div className="calculator-zero">
+      <button type="button" alt="">0</button>
+      <button type="button" alt="">.</button>
+    </div>
+  </div>
+);
+
+export default Digits;
