d2f66ead67b2827a25bf706cb2ade51297aa21dc
freddykaberuka <freddykaberuka2009@gmail.com>
2023-09-27 05:58:33 -0700

    add unit tests

---
+++ src/tests/operate.test.js
@@ -0,0 +1,22 @@
+import operate from '../logic/operate';
+
+describe('operate', () => {
+  it('should return a sum of two numbers', () => {
+    const result = operate('5', '3', '+');
+    expect(result).toBe('8');
+  });
+
+  it('should return a difference of two numbers', () => {
+    const result = operate('5', '3', '-');
+    expect(result).toBe('2');
+  });
+
+  it('should return a product of two numbers', () => {
+    const result = operate(5, 3, 'x');
+    expect(result).toBe('15');
+  });
+  it('should return a quotient of two numbers', () => {
+    const result = operate(6, 3, '÷');
+    expect(result).toBe('2');
+  });
+});
